c62c2fbc3e44d9804c27c253bf1018abe67ee7ab
PRIYA <76675024+priya-kumari-04@users.noreply.github.com>
2025-02-03 15:59:56 +0530

    Update SentimentAnalyzerEngine.py

--- SentimentAnalyzerEngine.py
+++ SentimentAnalyzerEngine.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 app = Flask(__name__)
 
-model_path = 'sentimentanalysis\\models\\model\\classifier.pkl'
+model_path = "sentimentanalysis/models/model/classifier.pkl"
 classifier = joblib.load(model_path)
 
 def predictfunc(review):    
